92fdac55bb222d74e9b5fc36dfcbc5774d298783
alexander <alexander8z@hotmail.com>
2017-11-25 23:19:22 -0500

    Correo de bienvenida Full funcionando se envia al actualizar los datos

--- Egresoft_Web/src/bean/EgresadoBean.java
+++ Egresoft_Web/src/bean/EgresadoBean.java
@@ -1,6 +1,5 @@
 package bean;
 
-
 import java.io.Serializable;
 import java.util.List;
 import java.util.Properties;
@@ -22,21 +21,19 @@ import dao.EgresadoDao;
 import entidades.Egresado;
 import mensajes.MessagesView;
 
-@ManagedBean (name="egresadoBean", eager = true )
+@ManagedBean(name = "egresadoBean", eager = true)
 @ViewScoped
 public class EgresadoBean implements Serializable {
 
-
 	private MessagesView msj = new MessagesView();
-	EgresadoDao daoEgresado=new EgresadoDao();
-	
+	EgresadoDao daoEgresado = new EgresadoDao();
+
 	private Egresado egresado = new Egresado();
-	private List<Egresado>listaEgresados;	
+	private List<Egresado> listaEgresados;
 	private Egresado egresadoAtributos;
 	private Egresado egresadoMail;
 	private long dniEgresado;
 	private String direccionEmail;
-
 
 	public String getDireccionEmail() {
 		return direccionEmail;
@@ -69,23 +66,23 @@ public class EgresadoBean implements Serializable {
 	public void setMsj(MessagesView msj) {
 		this.msj = msj;
 	}
-	
-	public Egresado bucarEmail() throws Exception{
-		
-		try{
-			
-			egresadoMail=daoEgresado.buscarMail(direccionEmail);
+
+	public Egresado bucarEmail() throws Exception {
+
+		try {
+
+			egresadoMail = daoEgresado.buscarMail(direccionEmail);
 			System.out.println("llego al bean");
-			
-			if(egresadoMail!=null){
-				
+
+			if (egresadoMail != null) {
+
 				System.out.println("instacio la clase");
-				
+
 				final String username = "pruebaegresado@gmail.com";
 				final String password = "Egresoft2017";
 				String contra;
-				
-				contra=egresadoMail.getContrasena();
+
+				contra = egresadoMail.getContrasena();
 
 				Properties props = new Properties();
 				props.put("mail.smtp.auth", "true");
@@ -106,57 +103,54 @@ public class EgresadoBean implements Serializable {
 					message.setFrom(new InternetAddress(username));
 					message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(direccionEmail));
 					message.setSubject("Recuperar Contraseña");
-					//message.setText("Texto de prueba," + "\n\n cuerpo del correo");
-					message.setText("Su contraseña es "+contra);
+					// message.setText("Texto de prueba," + "\n\n cuerpo del
+					// correo");
+					message.setText("Su contraseña es " + contra);
 
 					Transport.send(message);
 
 					System.out.println("enviado");
 					msj.info("Correo enviado exitosamente");
-					
-					
+
 				} catch (MessagingException e) {
 					throw new RuntimeException(e);
 				}
-				
-				
-			}else {
+
+			} else {
 				System.out.println("No instacio la clase");
 				msj.error("Correo no enviado exitosamente");
-				
+
 			}
-			
-		}catch (Exception e) {
-		
+
+		} catch (Exception e) {
+
 			throw e;
 		}
-		
-		return egresadoMail;	
-	}
-	
-	public Egresado bucarEgresado() throws Exception{
-		
-		try{
-			
-			egresadoAtributos=daoEgresado.buscarEgresado(dniEgresado);
-			
-			if(egresadoAtributos!=null){
-				
-				msj.info("Egresado en Base de datos");
-			}
-			else {
-				
-				msj.error("Egresado no existe en Base de Datos");
-			}
-		}catch (Exception e) {
-		
-			throw e;
-		}
-		
-		return egresadoAtributos;	
+
+		return egresadoMail;
 	}
 
-	
+	public Egresado bucarEgresado() throws Exception {
+
+		try {
+
+			egresadoAtributos = daoEgresado.buscarEgresado(dniEgresado);
+
+			if (egresadoAtributos != null) {
+
+				msj.info("Egresado en Base de datos");
+			} else {
+
+				msj.error("Egresado no existe en Base de Datos");
+			}
+		} catch (Exception e) {
+
+			throw e;
+		}
+
+		return egresadoAtributos;
+	}
+
 	public Egresado getEgresadoAtributos() {
 		return egresadoAtributos;
 	}
@@ -174,75 +168,128 @@ public class EgresadoBean implements Serializable {
 	}
 
 	public List<Egresado> getListaEgresados() {
-		
-		daoEgresado=new EgresadoDao();
+
+		daoEgresado = new EgresadoDao();
 		listaEgresados = daoEgresado.listaEgresados();
 
 		return listaEgresados;
 	}
 
 	public void setListaEgresados(List<Egresado> listaEgresados) {
-		
+
 		this.listaEgresados = listaEgresados;
 	}
 
-    
 	public void nuevoEgresado() {
 
-		try{
-			daoEgresado=new EgresadoDao();
+		try {
+			daoEgresado = new EgresadoDao();
 			daoEgresado.registrar(egresado);
-			egresado=new Egresado();
+			egresado = new Egresado();
 			msj.info("Egresado registrado exitosamente");
-			
-		}catch(Exception e){
-			
-			
+
+		} catch (Exception e) {
+
 		}
 	}
 
 	public void editarEgresadoAdmin() {
 
-		try{
-			daoEgresado=new EgresadoDao();
+		try {
+			daoEgresado = new EgresadoDao();
 			daoEgresado.editarEgresadoDesdeAdmin(egresado);
 			egresado = new Egresado();
 			msj.info("Informacion actualizada exitosamente");
-			
-		}catch (Exception e) {
-			
-		}
-		
-	}
-	
-	public void editarEgresado(){
-		
-		
-		if (daoEgresado.editaEgresadoFromEgresado(egresado)) {
-			System.out.println("editó al egresado");
-	        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Hecho", "Se registro al  egresado"));
-			
-		}else{
-			System.err.println("no lo edito");
-	        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "error", "no se pudo registrar al egresado"));
-		}
-		
+
+		} catch (Exception e) {
 
 		}
-	
+
+	}
+
+	public void editarEgresado() {
+
+		if (daoEgresado.editaEgresadoFromEgresado(egresado)) {
+			System.out.println("editó al egresado");
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "Hecho", "Se registro al  egresado"));
+			enviarEmail();
+
+		} else {
+			System.err.println("no lo edito");
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_WARN, "error", "no se pudo registrar al egresado"));
+		}
+
+	}
+
+	private void enviarEmail() {
+
+		Long id = egresado.getIdEgresado();
+		System.out.println("hola" + id);
+
+		egresadoAtributos = daoEgresado.buscarEgresado(id);
+
+		if (egresadoAtributos != null) {
+
+			System.out.println(egresadoAtributos.getEmailPrincipal());
+
+			final String username = "pruebaegresado@gmail.com";
+			final String password = "Egresoft2017";
+
+			Properties props = new Properties();
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.starttls.enable", "true");
+			props.put("mail.smtp.host", "smtp.gmail.com");
+			props.put("mail.smtp.port", "587");
+			props.put("mail.smtp.ssl.trust", "*");
+
+			Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(username, password);
+				}
+			});
+			String mjs = "Felicitaciones por su registro:\n";
+			mjs += "sus datos en la plataforma son :\n";
+			mjs+="\nTipo de documento : "+egresadoAtributos.getTipoDocumento()+"\n";
+			mjs+="Numero de documento : "+egresadoAtributos.getIdEgresado()+"\n";
+			mjs += "Nombres : " + egresadoAtributos.getNombres() + "\n";
+			mjs+="Apellidos : "+egresadoAtributos.getApellidos()+"\n";
+			mjs+="Nombre de Usuario para ingresar en la plataforma : "+egresadoAtributos.getEmailPrincipal()+"\n";
+			mjs+="Su contraseña para ingresar en la plataforma es : "+egresadoAtributos.getContrasena()+"\n";
+
+			try {
+
+				Message message = new MimeMessage(session);
+				message.setFrom(new InternetAddress(username));
+				message.setRecipients(Message.RecipientType.TO,InternetAddress.parse(egresadoAtributos.getEmailPrincipal()));
+				message.setSubject("Bienvenido a Egresoft ");
+				message.setText(mjs);
+
+			    Transport.send(message);
+
+				System.out.println("enviado");
+				msj.info("Correo enviado exitosamente");
+
+			} catch (MessagingException e) {
+				throw new RuntimeException(e);
+			}
+
+		}
+	}
 
 	public void eliminarEgresado() {
 
-		try{
-			daoEgresado=new EgresadoDao();
+		try {
+			daoEgresado = new EgresadoDao();
 			daoEgresado.eliminar(egresado);
 			egresado = new Egresado();
 			msj.info("Egresado eliminado exitosamente ");
-			
-		}catch(Exception e){
-			
+
+		} catch (Exception e) {
+
 		}
-		
+
 	}
 
 	public EgresadoDao getEgreDao() {
@@ -252,8 +299,5 @@ public class EgresadoBean implements Serializable {
 	public void setEgreDao(EgresadoDao daoEgresado) {
 		this.daoEgresado = daoEgresado;
 	}
-
-	
-	
 
 }
